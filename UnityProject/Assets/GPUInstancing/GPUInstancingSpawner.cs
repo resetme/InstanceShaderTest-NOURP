443de3c9c13ca92afdb2d142ca4598927c5892bd
Francisco Fernandez <fernandez-f@klab.com>
2020-09-24 18:00:14 +0900

    Files

---
+++ UnityProject/Assets/GPUInstancing/GPUInstancingSpawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPUInstancingSpawner : MonoBehaviour
+{
+    #region Public
+
+    [Header("Spawn Settings")] 
+    [Range(0,1023)]
+    public int amount;
+    
+    [Header("Mesh")]
+    public Mesh meshToSpawn;
+    
+    [Header("Render")]
+    public Shader gpuInstancedShader;
+
+    #endregion
+    
+    #region Private
+
+    private Material _sharedMaterial;
+    private Matrix4x4[] _matrices;
+    private MaterialPropertyBlock[] _block;
+    private int _shader_color = Shader.PropertyToID("_Color");
+    private Vector4[] _color;
+    #endregion
+
+    private void Start()
+    {
+        _sharedMaterial = new Material(gpuInstancedShader);
+        _sharedMaterial.enableInstancing = true;
+        
+        _matrices = new Matrix4x4[amount];
+        _color = new Vector4[amount];
+        
+        _block = new MaterialPropertyBlock[amount];
+        
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 position = Vector3.zero;;
+            position.x = Random.Range(-10, 10);
+            position.y= Random.Range(-10, 10);
+            position.z = Random.Range(-10, 10);
+            
+            Matrix4x4 matrix =  Matrix4x4.identity;
+            matrix.SetTRS(position, Quaternion.Euler(Vector3.zero), Vector3.one);
+
+            float r = Random.Range(0f, 1f);
+            float g = Random.Range(0f, 1f);
+            float b = Random.Range(0f, 1f);
+            _color[i] = new Vector4(r,g,b,1);  
+            
+            _block[i] = new MaterialPropertyBlock();
+            _block[i].SetColor(_shader_color, _color[i]);
+            
+            _matrices[i] = matrix;
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Graphics.DrawMesh(meshToSpawn, _matrices[i], _sharedMaterial, 0, null, 0, _block[i]);
+        }
+    }
+}
---
+++ UnityProject/Assets/GPUInstancing/UnlitIntanceShader.shader
@@ -0,0 +1,56 @@
+﻿Shader "Unlit/UnlitIntanceShader"
+{
+    Properties
+    {
+        _Color ("Color", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_instancing
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                half4 vertex : POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                half4 vertex : SV_POSITION;
+                 UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+            
+            UNITY_INSTANCING_BUFFER_START(Props)
+            UNITY_DEFINE_INSTANCED_PROP(half4, _Color)
+            UNITY_INSTANCING_BUFFER_END(Props)
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                
+                UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_TRANSFER_INSTANCE_ID(v, o);
+                
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            half4 frag (v2f i) : SV_Target
+            {
+                UNITY_SETUP_INSTANCE_ID(i);
+                half4 col = UNITY_ACCESS_INSTANCED_PROP(Props, _Color);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
